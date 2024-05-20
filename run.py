c4798ec66fc0f87f46437812d903be66b673eafe
notlober <selahattinbakidamar1@gmail.com>
2024-05-20 04:03:36 +0300

    Add files via upload

---
+++ run.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env cs_python
+
+# Copyright 2024 Cerebras Systems.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import argparse
+import json
+import numpy as np
+
+from cerebras.sdk.runtime.sdkruntimepybind import SdkRuntime, MemcpyDataType, MemcpyOrder # pylint: disable=no-name-in-module
+
+# Read arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('--name', help="the test compile output dir")
+parser.add_argument('--cmaddr', help="IP:port for CS system")
+args = parser.parse_args()
+
+# Get matrix dimensions from compile metadata
+with open(f"{args.name}/out.json", encoding='utf-8') as json_file:
+  compile_data = json.load(json_file)
+
+# Matrix dimensions
+N = int(compile_data['params']['N'])
+
+# Construct x, y
+x = np.arange(N, dtype=np.float32)
+y = np.arange(N, N*2, dtype=np.float32)
+
+# Calculate expected z
+z_expected = x + y
+
+# Construct a runner using SdkRuntime
+runner = SdkRuntime(args.name, cmaddr=args.cmaddr)
+
+# Get symbols for x, y, z on device
+x_symbol = runner.get_id('x')
+y_symbol = runner.get_id('y')
+z_symbol = runner.get_id('z')
+
+# Load and run the program
+runner.load()
+runner.run()
+
+# Copy x, y to device
+runner.memcpy_h2d(x_symbol, x, 0, 0, 1, 1, N, streaming=False,
+  order=MemcpyOrder.ROW_MAJOR, data_type=MemcpyDataType.MEMCPY_32BIT, nonblock=False)
+runner.memcpy_h2d(y_symbol, y, 0, 0, 1, 1, N, streaming=False,
+  order=MemcpyOrder.ROW_MAJOR, data_type=MemcpyDataType.MEMCPY_32BIT, nonblock=False)
+
+# Launch the compute function on device
+runner.launch('compute', nonblock=False)
+
+# Copy z back from device
+z_result = np.zeros([N], dtype=np.float32)
+runner.memcpy_d2h(z_result, z_symbol, 0, 0, 1, 1, N, streaming=False,
+  order=MemcpyOrder.ROW_MAJOR, data_type=MemcpyDataType.MEMCPY_32BIT, nonblock=False)
+
+# Stop the program
+runner.stop()
+
+# Ensure that the result matches our expectation
+np.testing.assert_allclose(z_result, z_expected, atol=0.01, rtol=0)
+print("SUCCESS!")
